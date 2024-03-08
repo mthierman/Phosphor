852456134f50fe659324d31b969580f87367d2c7
mthierman <mthierman@gmail.com>
2024-03-07 19:57:30 -0700

    Switch wallpapers on setting change

--- src/main.cxx
+++ src/main.cxx
@@ -12,24 +12,49 @@ auto check(HRESULT hr) -> void { std::println("{}", std::system_category().messa
 struct Window final : public glow::Window
 {
     auto WndProc(::UINT uMsg, ::WPARAM wParam, ::LPARAM lParam) -> ::LRESULT override;
+    auto on_create(::WPARAM wParam, ::LPARAM lParam) -> int;
     auto on_setting_change(::WPARAM wParam, ::LPARAM lParam) -> int;
+
+  private:
+    wil::com_ptr_t<IDesktopWallpaper, wil::err_exception_policy> m_wallpaper;
 };
 
 auto Window::WndProc(::UINT uMsg, ::WPARAM wParam, ::LPARAM lParam) -> ::LRESULT
 {
     switch (uMsg)
     {
+        case WM_CREATE: return on_create(wParam, lParam);
         case WM_SETTINGCHANGE: return on_setting_change(wParam, lParam);
     }
 
     return ::DefWindowProcA(m_hwnd.get(), uMsg, wParam, lParam);
 }
 
+auto Window::on_create(::WPARAM wParam, ::LPARAM lParam) -> int
+{
+    m_wallpaper = wil::CoCreateInstance<IDesktopWallpaper>(CLSID_DesktopWallpaper, CLSCTX_ALL);
+
+    return 0;
+}
+
 auto Window::on_setting_change(::WPARAM wParam, ::LPARAM lParam) -> int
 {
-    auto mode{is_dark()};
+    auto isDark{is_dark()};
 
-    if (mode) { OutputDebugStringA("true!"); }
+    unsigned int count;
+    check(m_wallpaper->GetMonitorDevicePathCount(&count));
+
+    LPWSTR monitor;
+    check(m_wallpaper->GetMonitorDevicePathAt(0, &monitor));
+
+    auto pictures{glow::known_folder(FOLDERID_Pictures)};
+
+    auto images{std::make_pair(pictures / "wallpapers" / "dark.jpg",
+                               pictures / "wallpapers" / "light.jpg")};
+
+    if (isDark) { m_wallpaper->SetWallpaper(0, images.first.wstring().c_str()); }
+
+    else { m_wallpaper->SetWallpaper(0, images.second.wstring().c_str()); }
 
     return 0;
 }
@@ -37,26 +62,11 @@ auto Window::on_setting_change(::WPARAM wParam, ::LPARAM lParam) -> int
 auto main() -> int
 {
     glow::CoInitialize coInit;
+
     glow::App app;
+
     Window window;
     window.create_window();
-    // HRESULT hr;
-
-    // auto wallpaper = wil::CoCreateInstance<IDesktopWallpaper>(CLSID_DesktopWallpaper,
-    // CLSCTX_ALL);
-
-    // unsigned int count;
-    // check(wallpaper->GetMonitorDevicePathCount(&count));
-
-    // LPWSTR monitor;
-    // check(wallpaper->GetMonitorDevicePathAt(0, &monitor));
-
-    // auto pictures{glow::known_folder(FOLDERID_Pictures)};
-
-    // auto images{std::make_pair(pictures / "wallpapers" / "dark.jpg",
-    //                            pictures / "wallpapers" / "light.jpg")};
-
-    // check(wallpaper->SetWallpaper(0, images.first.wstring().c_str()));
 
     return app();
 }
