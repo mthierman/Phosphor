08f0d20274652c31fcdff55352dbc7093ee65973
mthierman <mthierman@gmail.com>
2025-04-03 23:52:50 -0600

    Simplify window

--- phosphor/src/console.cxx
+++ phosphor/src/console.cxx
@@ -1,35 +1,5 @@
 #include <Windows.h>
 #include <cstdlib>
-#include <expected>
-#include <system_error>
-
-auto last_error() -> std::error_code {
-    return std::error_code(GetLastError(), std::system_category());
-}
-
-auto module_handle() -> std::expected<HMODULE, std::error_code> {
-    HMODULE hmodule;
-
-    if (GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT
-                               | GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
-                           reinterpret_cast<LPCWSTR>(&module_handle),
-                           &hmodule)
-        == 0) {
-        return std::unexpected(last_error());
-    };
-
-    return hmodule;
-}
-
-auto application_icon() -> HICON {
-    return static_cast<HICON>(
-        LoadImageW(nullptr, IDI_APPLICATION, IMAGE_ICON, 0, 0, LR_SHARED | LR_DEFAULTSIZE));
-}
-
-auto arrow_cursor() -> HCURSOR {
-    return static_cast<HCURSOR>(
-        LoadImageW(nullptr, IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_SHARED | LR_DEFAULTSIZE));
-}
 
 auto CALLBACK window_procedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) -> LRESULT {
     // if (msg == WM_NCCREATE) {
@@ -45,18 +15,42 @@ auto CALLBACK window_procedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam
 }
 
 auto wmain(int /* argc */, wchar_t* /* argv */[], wchar_t* /* envp */[]) -> int {
+    auto instance = []() -> HMODULE {
+        HMODULE hmodule;
+
+        if (GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT
+                                   | GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
+                               reinterpret_cast<LPCWSTR>(&window_procedure),
+                               &hmodule)
+            == 0) {
+            return nullptr;
+        };
+
+        return hmodule;
+    }();
+
+    auto icon = []() -> HICON {
+        return static_cast<HICON>(
+            LoadImageW(nullptr, IDI_APPLICATION, IMAGE_ICON, 0, 0, LR_SHARED | LR_DEFAULTSIZE));
+    }();
+
+    auto cursor = []() -> HCURSOR {
+        return static_cast<HCURSOR>(
+            LoadImageW(nullptr, IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_SHARED | LR_DEFAULTSIZE));
+    }();
+
     WNDCLASSEXW window_class { .cbSize { sizeof(WNDCLASSEXW) },
                                .style { 0 },
                                .lpfnWndProc { window_procedure },
                                .cbClsExtra { 0 },
                                .cbWndExtra { 0 },
-                               .hInstance { module_handle().value_or(nullptr) },
-                               .hIcon { application_icon() },
-                               .hCursor { arrow_cursor() },
+                               .hInstance { instance },
+                               .hIcon { icon },
+                               .hCursor { cursor },
                                .hbrBackground { nullptr },
                                .lpszMenuName { nullptr },
                                .lpszClassName { L"MessageWindow" },
-                               .hIconSm { application_icon() } };
+                               .hIconSm { icon } };
 
     HWND window_handle;
 
