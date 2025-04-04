d929467b77510351cf74262befd9569975befe84
mthierman <mthierman@gmail.com>
2025-04-03 22:40:14 -0600

    Message loop in win32 app

--- phosphor/src/win32.cxx
+++ phosphor/src/win32.cxx
@@ -5,5 +5,19 @@ auto WINAPI wWinMain(HINSTANCE /* hinstance */,
                      HINSTANCE /* hprevinstance */,
                      PWSTR /* pcmdline */,
                      int /* ncmdshow */) -> int {
-    return EXIT_SUCCESS;
+    MSG msg {};
+    int r {};
+
+    while ((r = GetMessageW(&msg, nullptr, 0, 0)) != 0) {
+        if (r == -1) {
+            return EXIT_FAILURE;
+        }
+
+        else {
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+    }
+
+    return static_cast<int>(msg.wParam);
 }
