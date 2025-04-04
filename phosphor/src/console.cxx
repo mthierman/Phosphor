0f068086eaad91df1d4f19489cca19b127fe117d
mthierman <mthierman@gmail.com>
2025-04-03 22:39:50 -0600

    Message loop in console app

--- phosphor/src/console.cxx
+++ phosphor/src/console.cxx
@@ -2,5 +2,19 @@
 #include <cstdlib>
 
 auto wmain(int /* argc */, wchar_t* /* argv */[], wchar_t* /* envp */[]) -> int {
-    return EXIT_SUCCESS;
+    MSG msg {};
+    int r {};
+
+    while ((r = GetMessageW(&msg, nullptr, 0, 0)) != 0) {
+        if (r == -1) {
+            return EXIT_FAILURE;
+        }
+
+        else {
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+    }
+
+    return static_cast<int>(msg.wParam);
 }
