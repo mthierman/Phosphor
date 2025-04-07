56a5ef803a451985992fab214fb520a9bf60ae47
mthierman <mthierman@gmail.com>
2025-04-07 10:47:32 -0600

    Don't null check message_handler, we guarantee default

--- phosphor/src/phosphor.cpp
+++ phosphor/src/phosphor.cpp
@@ -11,26 +11,28 @@ auto app::run(this Self& self) -> int {
     self.desktop_wallpaper->GetMonitorDevicePathCount(&self.count);
     self.desktop_wallpaper->GetMonitorDevicePathAt(0, &self.monitor);
 
-    auto wndproc = [&self](HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) -> LRESULT {
-        if (msg == WM_SETTINGCHANGE) {
-            self.config.load();
+    // auto wndproc = [&self](HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) -> LRESULT {
+    //     if (msg == WM_SETTINGCHANGE) {
+    //         self.config.load();
 
-            if (pane::color(winrt::UIColorType::Background).is_dark()) {
-                self.theme = phosphor::theme::dark;
-                self.desktop_wallpaper->SetWallpaper(0, self.config.settings.dark.c_str());
-            } else {
-                self.theme = phosphor::theme::light;
-                self.desktop_wallpaper->SetWallpaper(0, self.config.settings.light.c_str());
-            }
+    //         if (pane::color(winrt::UIColorType::Background).is_dark()) {
+    //             self.theme = phosphor::theme::dark;
+    //             self.desktop_wallpaper->SetWallpaper(0, self.config.settings.dark.c_str());
+    //         } else {
+    //             self.theme = phosphor::theme::light;
+    //             self.desktop_wallpaper->SetWallpaper(0, self.config.settings.light.c_str());
+    //         }
 
-            pane::debug(self.config.settings.dark);
-            pane::debug(self.config.settings.light);
-        }
+    //         pane::debug(self.config.settings.dark);
+    //         pane::debug(self.config.settings.light);
+    //     }
 
-        return DefWindowProcW(hwnd, msg, wparam, lparam);
-    };
+    //     return DefWindowProcW(hwnd, msg, wparam, lparam);
+    // };
 
-    self.window = std::make_unique<pane::window>(std::move(wndproc));
+    // self.window = std::make_unique<pane::window>(std::move(wndproc));
+
+    self.window = std::make_unique<pane::window>();
 
     return pane::system::message_loop();
 };
