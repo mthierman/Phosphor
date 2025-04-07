e62a2858429c2dad4169e78729acda125a048b5c
mthierman <mthierman@gmail.com>
2025-04-07 10:23:57 -0600

    Fix wndproc lambda

--- phosphor/src/phosphor.cpp
+++ phosphor/src/phosphor.cpp
@@ -4,5 +4,43 @@
 #include <pane/pane.hxx>
 
 namespace phosphor {
+app::app() {
+    config.load();
+
+    auto wndproc = [](HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) -> LRESULT {
+        if (msg == WM_SETTINGCHANGE) {
+            // config.load();
+
+            auto desktop_wallpaper { wil::CoCreateInstance<IDesktopWallpaper>(
+                CLSID_DesktopWallpaper, CLSCTX_ALL) };
+            UINT count;
+            LPWSTR monitor;
+
+            desktop_wallpaper->GetMonitorDevicePathCount(&count);
+            desktop_wallpaper->GetMonitorDevicePathAt(0, &monitor);
+
+            if (phosphor::system_dark_mode()) {
+                // theme = phosphor::theme::dark;
+            } else {
+                // theme = phosphor::theme::light;
+            }
+
+            // switch (theme) {
+            //     case phosphor::theme::dark: {
+            //         pane::debug(config.settings.dark);
+            //         desktop_wallpaper->SetWallpaper(0, config.settings.dark.c_str());
+            //     } break;
+            //     case phosphor::theme::light: {
+            //         pane::debug(config.settings.light);
+            //         desktop_wallpaper->SetWallpaper(0, config.settings.light.c_str());
+            //     } break;
+            // }
+        }
+
+        return DefWindowProcW(hwnd, msg, wparam, lparam);
+    };
+
+    window = std::make_unique<pane::window>(wndproc);
+};
 auto system_dark_mode() -> bool { return pane::color(winrt::UIColorType::Background).is_dark(); }
 } // namespace phosphor
--- phosphor/src/phosphor.hpp
+++ phosphor/src/phosphor.hpp
@@ -3,6 +3,7 @@
 #include <ShObjIdl.h>
 #include <filesystem>
 #include <cstdlib>
+#include <memory>
 #include <utility>
 #include <print>
 #include <wil/com.h>
@@ -22,12 +23,20 @@ struct settings final {
     std::filesystem::path light;
 };
 struct app final {
+    using Self = app;
+
+    app();
+    ~app() = default;
+
+    pane::config<phosphor::settings> config { pane::config<phosphor::settings>() };
+    phosphor::theme theme { phosphor::theme::light };
     std::filesystem::path config_file {
         pane::filesystem::known_folder()
             .transform([](const std::filesystem::path& path) -> std::filesystem::path {
         return path / u"Phosphor" / u"config.json";
     }).value_or(u"")
     };
+    std::unique_ptr<pane::window> window;
 };
 
 auto system_dark_mode() -> bool;
