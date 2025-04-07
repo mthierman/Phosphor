c632a781ab1faeda3fd346f0f19796a0b85d5499
mthierman <mthierman@gmail.com>
2025-04-07 15:34:22 -0600

    Move webview inside window

--- phosphor/src/phosphor.cpp
+++ phosphor/src/phosphor.cpp
@@ -12,7 +12,7 @@ phosphor::phosphor() {
 auto phosphor::app() -> int {
     auto self { Self() };
 
-    pane::window({ .title { u8"title" }, .visible { false }, .webview { false } },
+    pane::window({ .title { u8"title" }, .visible { false }, .webview { true } },
                  [&self](HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) -> LRESULT {
         if (msg == WM_SETTINGCHANGE) {
             self.config.load();
