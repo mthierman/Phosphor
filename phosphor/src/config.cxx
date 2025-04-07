729d8eacca8640587f83d9b57a71a94eed5c9f98
mthierman <mthierman@gmail.com>
2025-04-06 18:31:38 -0600

    Fix config

--- phosphor/src/config.cxx
+++ phosphor/src/config.cxx
@@ -2,16 +2,15 @@
 
 namespace phosphor {
 auto config::load(this Self& self) -> void {
-    auto config_file { self.paths.at(u8"config_file") };
     auto ec { glz::read_file_json(self.settings,
-                                  reinterpret_cast<const char*>(config_file.u8string().data()),
+                                  reinterpret_cast<const char*>(self.config_file.u8string().data()),
                                   std::string {}) };
 }
 
 auto config::save(this const Self& self) -> void {
-    auto config_file { self.paths.at(u8"config_file") };
-    auto ec { glz::write_file_json(self.settings,
-                                   reinterpret_cast<const char*>(config_file.u8string().data()),
-                                   std::string {}) };
+    auto ec { glz::write_file_json(
+        self.settings,
+        reinterpret_cast<const char*>(self.config_file.u8string().data()),
+        std::string {}) };
 }
 } // namespace phosphor
