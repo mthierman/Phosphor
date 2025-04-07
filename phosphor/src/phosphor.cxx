2831da9f352894d23a9869ffa44f413e15af5ffc
mthierman <mthierman@gmail.com>
2025-04-06 22:37:17 -0600

    Move make_window to header

--- phosphor/src/phosphor.cxx
+++ phosphor/src/phosphor.cxx
@@ -37,71 +37,13 @@ auto window_procedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) -> LRES
     return DefWindowProcW(hwnd, msg, wparam, lparam);
 }
 
-auto make_window() -> void {
-    auto instance = []() -> HMODULE {
-        HMODULE hmodule;
-
-        if (GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT
-                                   | GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
-                               reinterpret_cast<LPCWSTR>(&window_procedure),
-                               &hmodule)
-            == 0) {
-            return nullptr;
-        };
-
-        return hmodule;
-    }();
-
-    auto icon = []() -> HICON {
-        return static_cast<HICON>(
-            LoadImageW(nullptr, IDI_APPLICATION, IMAGE_ICON, 0, 0, LR_SHARED | LR_DEFAULTSIZE));
-    }();
-
-    auto cursor = []() -> HCURSOR {
-        return static_cast<HCURSOR>(
-            LoadImageW(nullptr, IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_SHARED | LR_DEFAULTSIZE));
-    }();
-
-    WNDCLASSEXW window_class { .cbSize { sizeof(WNDCLASSEXW) },
-                               .style { 0 },
-                               .lpfnWndProc { window_procedure },
-                               .cbClsExtra { 0 },
-                               .cbWndExtra { 0 },
-                               .hInstance { instance },
-                               .hIcon { icon },
-                               .hCursor { cursor },
-                               .hbrBackground { nullptr },
-                               .lpszMenuName { nullptr },
-                               .lpszClassName { L"MessageWindow" },
-                               .hIconSm { icon } };
-
-    if (GetClassInfoExW(instance, window_class.lpszClassName, &window_class) == 0) {
-        RegisterClassExW(&window_class);
-    }
-
-    [[maybe_unused]] auto window { CreateWindowExW(0,
-                                                   window_class.lpszClassName,
-                                                   window_class.lpszClassName,
-                                                   WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
-                                                   CW_USEDEFAULT,
-                                                   CW_USEDEFAULT,
-                                                   CW_USEDEFAULT,
-                                                   CW_USEDEFAULT,
-                                                   nullptr,
-                                                   nullptr,
-                                                   window_class.hInstance,
-                                                   nullptr) };
-
-    // ShowWindow(window, SW_SHOWNORMAL);
-}
-
 auto wWinMain(HINSTANCE /* hinstance */,
               HINSTANCE /* hprevinstance */,
               PWSTR /* pcmdline */,
               int /* ncmdshow */) -> int {
     auto co_init { pane::co_init::apartment_threaded() };
     config.load();
-    make_window();
+    phosphor::make_window();
 
     return pane::system::message_loop();
 }
--- phosphor/src/phosphor.hxx
+++ phosphor/src/phosphor.hxx
@@ -19,6 +19,64 @@ namespace phosphor {
 enum struct theme { light = 0, dark };
 
 auto system_dark_mode() -> bool { return pane::color(winrt::UIColorType::Background).is_dark(); }
+
+auto make_window() -> void {
+    auto instance = []() -> HMODULE {
+        HMODULE hmodule;
+
+        if (GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT
+                                   | GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
+                               reinterpret_cast<LPCWSTR>(&window_procedure),
+                               &hmodule)
+            == 0) {
+            return nullptr;
+        };
+
+        return hmodule;
+    }();
+
+    auto icon = []() -> HICON {
+        return static_cast<HICON>(
+            LoadImageW(nullptr, IDI_APPLICATION, IMAGE_ICON, 0, 0, LR_SHARED | LR_DEFAULTSIZE));
+    }();
+
+    auto cursor = []() -> HCURSOR {
+        return static_cast<HCURSOR>(
+            LoadImageW(nullptr, IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_SHARED | LR_DEFAULTSIZE));
+    }();
+
+    WNDCLASSEXW window_class { .cbSize { sizeof(WNDCLASSEXW) },
+                               .style { 0 },
+                               .lpfnWndProc { window_procedure },
+                               .cbClsExtra { 0 },
+                               .cbWndExtra { 0 },
+                               .hInstance { instance },
+                               .hIcon { icon },
+                               .hCursor { cursor },
+                               .hbrBackground { nullptr },
+                               .lpszMenuName { nullptr },
+                               .lpszClassName { L"MessageWindow" },
+                               .hIconSm { icon } };
+
+    if (GetClassInfoExW(instance, window_class.lpszClassName, &window_class) == 0) {
+        RegisterClassExW(&window_class);
+    }
+
+    [[maybe_unused]] auto window { CreateWindowExW(0,
+                                                   window_class.lpszClassName,
+                                                   window_class.lpszClassName,
+                                                   WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
+                                                   CW_USEDEFAULT,
+                                                   CW_USEDEFAULT,
+                                                   CW_USEDEFAULT,
+                                                   CW_USEDEFAULT,
+                                                   nullptr,
+                                                   nullptr,
+                                                   window_class.hInstance,
+                                                   nullptr) };
+
+    // ShowWindow(window, SW_SHOWNORMAL);
+}
 } // namespace phosphor
 
 template <> struct glz::meta<phosphor::theme> {
