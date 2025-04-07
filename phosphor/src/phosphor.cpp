32e3fbea41d1e39f07ae926649416fb9bfec9d65
mthierman <mthierman@gmail.com>
2025-04-07 10:35:36 -0600

    Cleanup window procedure

--- phosphor/src/phosphor.cpp
+++ phosphor/src/phosphor.cpp
@@ -7,29 +7,23 @@ namespace phosphor {
 app::app() {
     config.load();
 
+    this->desktop_wallpaper->GetMonitorDevicePathCount(&this->count);
+    this->desktop_wallpaper->GetMonitorDevicePathAt(0, &this->monitor);
+
     auto wndproc = [this](HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) -> LRESULT {
         if (msg == WM_SETTINGCHANGE) {
             this->config.load();
 
-            this->desktop_wallpaper->GetMonitorDevicePathCount(&this->count);
-            this->desktop_wallpaper->GetMonitorDevicePathAt(0, &this->monitor);
-
             if (this->system_dark_mode()) {
                 this->theme = phosphor::theme::dark;
+                desktop_wallpaper->SetWallpaper(0, config.settings.dark.c_str());
             } else {
                 this->theme = phosphor::theme::light;
+                desktop_wallpaper->SetWallpaper(0, config.settings.light.c_str());
             }
 
-            switch (theme) {
-                case phosphor::theme::dark: {
-                    pane::debug(this->config.settings.dark);
-                    desktop_wallpaper->SetWallpaper(0, config.settings.dark.c_str());
-                } break;
-                case phosphor::theme::light: {
-                    pane::debug(this->config.settings.light);
-                    desktop_wallpaper->SetWallpaper(0, config.settings.light.c_str());
-                } break;
-            }
+            pane::debug(this->config.settings.dark);
+            pane::debug(this->config.settings.light);
         }
 
         return DefWindowProcW(hwnd, msg, wparam, lparam);
