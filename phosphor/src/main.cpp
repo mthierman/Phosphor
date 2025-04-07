7824411da422a9e93bd2bbb4c28d0982c85dd89a
mthierman <mthierman@gmail.com>
2025-04-07 11:55:04 -0600

    Fix build

--- phosphor/src/main.cpp
+++ phosphor/src/main.cpp
@@ -4,7 +4,5 @@ auto wWinMain(HINSTANCE /* hinstance */,
               HINSTANCE /* hprevinstance */,
               PWSTR /* pcmdline */,
               int /* ncmdshow */) -> int {
-    // auto phosphor { phosphor::init() };
-
     return phosphor::run();
 }
